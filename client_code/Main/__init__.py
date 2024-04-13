13af93bc13c0d675680e673a056b2e63e4b67b1e
zikreaarti1998@gmail.com <zikreaarti1998@gmail.com>
2024-04-13 14:39:57 +0000

    Edited Main, settings, Home.RowTemplate1 and Home; Deleted Pricing

--- client_code/Main/__init__.py
+++ client_code/Main/__init__.py
@@ -4,11 +4,10 @@ import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-from ..Home import Home
 from ..About import About
 from ..Contact import Contact
-from ..Pricing import Pricing
 from ..FAQ import FAQ
+from ..Home import Home
 
 
 class Main(MainTemplate):
@@ -40,11 +39,6 @@ class Main(MainTemplate):
   def bottom_contact_link_click(self, **event_args):
     """This method is called when the link is clicked"""
     self.contact_link_click()
-
-  def pricing_link_click(self, **event_args):
-    """This method is called when the link is clicked"""
-    self.content_panel.clear()
-    self.content_panel.add_component(Pricing(), full_width_row=True)
 
   def faq_link_click(self, **event_args):
     """This method is called when the link is clicked"""
