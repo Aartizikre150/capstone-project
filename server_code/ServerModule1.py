b4d90d2853484f663fea00ceb93bb17092183013
zikreaarti1998@gmail.com <zikreaarti1998@gmail.com>
2024-04-16 00:17:00 +0000

    Edited settings, ServerModule1 and Home

--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -12,3 +12,10 @@ def add_contact_info(name, email, topic, question):
   anvil.email.send(from_name="Contact Form", 
                    subject="New Web Contact",
                    text=f"New web contact from {name} ({email})\nTopic: {topic}\nComment/Question: {question}")
+
+@anvil.server.callable
+def add_ecommerce_data_info(super_category,main_category,sub_category1,sub_category2,brand,product_name,discounted_price,email):
+  app_tables.ecommerce_data.add_row(super_category=super_category, main_category=main_category, sub_category1=sub_category1, sub_category2=sub_category2, product_name = product_name, brand = brand,discounted_price = discounted_price, email = email,time=datetime.now())
+  anvil.email.send(from_name="ICT Ignite", 
+                   subject="New Ecommerce return inquery ",
+                   text=f"Ecommerce return inquery\n {main_category} ({super_category})\nbrand: {brand}\n Product name: {product_name}\nPrice: {discounted_price}")
